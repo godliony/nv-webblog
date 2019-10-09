4bf79c58f17446bb51e992aca94d460a79aaa964
godliny <godliony@gmail.com>
2019-10-09 16:23:30 +0700

    Modify routes format MVC

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello nodejs server')
+let express = require('express')
+let bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require('./routes')(app)
+
+let port = 8081
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`)
+})
---
+++ server/src/routes.js
@@ -0,0 +1,16 @@
+const UserController = require('./controllers/userController')
+
+module.exports = (app) => {
+  /*RESTFUL API for users management*/
+  //create user
+  app.post('/user', UserController.create)
+  // edit user, suspend, active
+  app.put('/user/:userId', UserController.put)
+  //detele user
+  app.delete('/user/:userId', UserController.remove)
+  //get user by id
+  app.get('/user/:userId', UserController.show)
+  //get all user
+  app.get('/users', UserController.index)
+
+}
