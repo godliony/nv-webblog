7ea68461faf69929cb37cf5d80581ec29417bcea
godliny <godliony@gmail.com>
2019-10-10 10:03:04 +0700

    Install sqlite3 and sequelize(Use for create database) add config folder and models(in models are table database)

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,17 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+const { sequelize } = require('./models')
 const app = express()
+const config = require('./config/config')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 require('./routes')(app)
 
-let port = 8081
-
-app.listen(port, () => {
-  console.log(`server running on ${port}`)
+let port = process.env.PORT || config.port
+sequelize.sync({ force: false }).then(() => {
+  app.listen(port, () => {
+    console.log(`server running on ${port}`)
+  })
 })
+
